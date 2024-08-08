edffa8628f6f6edb7eb8682fcf62037a60fc862e
Gabriel Cornide <gabrielcornide.pg@gmail.com>
2024-08-08 14:50:43 -0300

    component header/nav complet

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import { Header } from "./Header";
+import { NavResponsive } from "./NavResponsive";
+import { Nav } from "./Nav";
+
+export { Header, NavResponsive, Nav };
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,9 +1,9 @@
+import { Header } from "../components/Header";
 export const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <Header />
+      <h1 className="text-3xl font-bold text-red-500">Hola</h1>
     </>
   );
 };
